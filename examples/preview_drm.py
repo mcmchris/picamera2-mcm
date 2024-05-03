5bc210646c495ed9a331d9b5f21cc79ab2d81a95
Christopher Méndez <mcmchris@hotmail.com>
2024-05-03 18:37:52 -0400

    preview_drm update

--- examples/preview_drm.py
+++ examples/preview_drm.py
@@ -7,7 +7,7 @@ import time
 from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
-picam2.start_preview(Preview.DRM, x=100, y=100, width=640, height=480)
+picam2.start_preview(Preview.DRM, x=0, y=0, width=1920, height=1080)
 
 preview_config = picam2.create_preview_configuration({"size": (640, 360)})
 picam2.configure(preview_config)
